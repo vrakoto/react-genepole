310b1a6cd1f9127293c5e9cf41a7e813a755d370
carellien <carellien.rakotoarisoa@gmail.com>
2022-12-02 00:39:40 +0100

    petite modif

--- src/views/App.js
+++ src/views/App.js
@@ -12,11 +12,12 @@ function App() {
     const [updateCellule, setUpdateCellule] = useState(0)
     const [enableFusion, setEnableFusion] = useState(false)
     const [enableMixeur, setEnableMixeur] = useState(false)
+    const temperatureVal = useRef(0)
 
     const fermerModal = () => {
         setOpenModal(false)
         setEnableMovingXY(false)
-        // setSelectedGoutte('')
+        setSelectedGoutte('')
     }
 
     /**
@@ -41,9 +42,35 @@ function App() {
                 const newPosition = colonnes[positionX].children[positionY]
                 newPosition.classList.add('item') // Puis on ajoute 
                 newPosition.style.backgroundColor = selectedGoutte.color
-                
+
                 // Puis on récupère la cardinalité de la nouvelle goutte
                 // pour ensuite pouvoir la repositionner ailleurs sans devoir la sélectionner à chaque fois 
+                const idColonneNewPosition = parseInt(newPosition.parentNode.id.replace(/\D/g, ""))
+                const idCelluleNewPosition = parseInt(newPosition.id.replace(/\D/g, ""))
+                setSelectedGoutte(prev => ({ ...prev, idColonne: idColonneNewPosition, idCellule: idCelluleNewPosition, color: selectedGoutte.color }))
+                setUpdateCellule(prev => prev + 1)
+            } catch (error) {
+                console.log(error);
+                alert("Cette cardinalité n'existe pas")
+            }
+        } else {
+            alert('Sélectionnez une goutte présente dans une cellule !')
+        }
+    }
+
+    // meme méthode que bougeXY mais bouge uniquement dans la plaque mixeur
+    const bougerZoneMixante = () => {
+        const colonnes = document.querySelectorAll('.colonne')
+        if (selectedGoutte.color !== '' && selectedGoutte.color !== undefined) {
+            try {
+                const currentPosition = colonnes[selectedGoutte.idColonne - 1].children[selectedGoutte.idCellule - 1]
+                currentPosition.classList.remove('item')
+                currentPosition.style.backgroundColor = ''
+
+                const newPosition = colonnes[1].children[8]
+                newPosition.classList.add('item')
+                newPosition.style.backgroundColor = selectedGoutte.color
+
                 const idColonneNewPosition = parseInt(newPosition.parentNode.id.replace(/\D/g, ""))
                 const idCelluleNewPosition = parseInt(newPosition.id.replace(/\D/g, ""))
                 setSelectedGoutte(prev => ({ ...prev, idColonne: idColonneNewPosition, idCellule: idCelluleNewPosition, color: selectedGoutte.color }))
@@ -55,6 +82,22 @@ function App() {
             alert('Sélectionnez une goutte présente dans une cellule !')
         }
     }
+
+    /* const test = (currentPosition, newPosition) => {
+        const colonnes = document.querySelectorAll('.colonne')
+        const currentPosition = colonnes[selectedGoutte.idColonne - 1].children[selectedGoutte.idCellule - 1]
+        currentPosition.classList.remove('item')
+        currentPosition.style.backgroundColor = ''
+
+        const newPosition = colonnes[1].children[8]
+        newPosition.classList.add('item')
+        newPosition.style.backgroundColor = selectedGoutte.color
+
+        const idColonneNewPosition = parseInt(newPosition.parentNode.id.replace(/\D/g, ""))
+        const idCelluleNewPosition = parseInt(newPosition.id.replace(/\D/g, ""))
+        setSelectedGoutte(prev => ({ ...prev, idColonne: idColonneNewPosition, idCellule: idCelluleNewPosition, color: selectedGoutte.color }))
+        setUpdateCellule(prev => prev + 1)
+    } */
 
 
     const supprimerGoutte = () => {
@@ -90,10 +133,6 @@ function App() {
         setUpdateCellule(prev => prev + 1)
     }
 
-    const bougerFusion = () => {
-
-    }
-
     /**
      * Retire toutes les gouttes présente dans le tableau
      */
@@ -122,15 +161,15 @@ function App() {
             if (updateCellule >= 0) {
                 lesCellules.forEach((cel, key) => {
                     if (cel.classList.contains('item')) {
-                        if (lesCellules[key+1].classList.contains('item')) {
+                        if (lesCellules[key + 1].classList.contains('item')) {
                             cel.classList.add('fusion')
                             cel.nextSibling.classList.add('fusion')
                             cel.nextSibling.style.backgroundColor = cel.style.backgroundColor
                         }
-                        if (lesCellules[key+10].classList.contains('item')) {
+                        if (lesCellules[key + 10].classList.contains('item')) {
                             cel.classList.add('fusion')
-                            lesCellules[key+10].classList.add('fusion')
-                            lesCellules[key+10].style.backgroundColor = cel.style.backgroundColor
+                            lesCellules[key + 10].classList.add('fusion')
+                            lesCellules[key + 10].style.backgroundColor = cel.style.backgroundColor
                         }
                     }
                 });
@@ -144,12 +183,16 @@ function App() {
             const plaques_mixeur = document.querySelectorAll('.mixeur')
             plaques_mixeur.forEach((plaque, key) => {
                 if (plaque.classList.contains('item')) {
+                    let couleurPlaque = ''
                     switch (plaque.style.backgroundColor) {
-                        case 'green': plaque.style.backgroundColor = "#FF7700"; break;
-                        case 'blue': plaque.style.backgroundColor = "#804000"; break;
-                        case 'yellow': plaque.style.backgroundColor = "#800080"; break;
-                        case 'red': plaque.style.backgroundColor = "#ff0000"; break;
+                        case 'green': couleurPlaque = "#FF7700"; break;
+                        case 'blue': couleurPlaque = "#804000"; break;
+                        case 'yellow': couleurPlaque = "#800080"; break;
+                        case 'red': couleurPlaque = "#ff0000"; break;
                     }
+                    setTimeout(() => {
+                        plaque.style.backgroundColor = couleurPlaque
+                    }, temperatureVal.current.value * 1000);
                 }
             })
         }
@@ -173,33 +216,8 @@ function App() {
     }, [enableMixeur])
 
     const lancer = () => {
-        /* let i = 0
-        const lesColonnes = document.querySelectorAll('.colonne')
         const lesCellules = document.querySelectorAll('.cellule')
-
-        const positionX = (selectedGoutte.idColonne - 1)
-        const positionY = (selectedGoutte.idCellule - 1)
-
-        const cellules = document.querySelectorAll('.cellule') */
-
-        /* const path = ["haut", "droite", "bas", "gauche"]
-
-        cellules.forEach((cellule, key) => {
-            let j = key
-
-            if (getComputedStyle(cellule).background !== 'none') {
-                setInterval(() => {
-                    let randomPath = path[Math.floor(Math.random() * path.length)]
-                    switch (randomPath) {
-                        case "haut": j--; break;
-                        case "droite": j += 10; break;
-                        case "bas": j++; break;
-                        case "gauche": j -= 10; break;
-                    }
-                    cellules[j].style.backgroundColor = getComputedStyle(cellule).backgroundColor
-                }, 1000)
-            }
-        }); */
+        const path = ["haut", "droite", "bas", "gauche"]
     }
 
     return (
@@ -234,6 +252,9 @@ function App() {
                         <div className="mt-3"></div>
                         <button className="btn btn-secondary" onClick={fermerModal}>Fermer</button>
                         <button className="btn btn-success ms-2" onClick={bougerXY}>Bouger</button>
+                        {enableMixeur ? (
+                            <button className="btn btn-warning ms-2" onClick={bougerZoneMixante}>Déplacer dans zone mixeur</button>
+                        ) : ''}
                         <button className="btn btn-danger ms-2" onClick={supprimerGoutte}>Retirer</button>
                     </>
                 )}
@@ -246,14 +267,15 @@ function App() {
                 </div>
 
                 <div className="mt-2"></div>
-                {enableFusion ? (
-                    <button className="btn btn-success" onClick={bougerFusion}>Intéragir avec une fusion</button>
-                ) : ''}
 
                 <div className="form-check form-switch">
                     <input className="form-check-input" type="checkbox" role="switch" id="activerMixeur" onChange={activerMixeur} />
                     <label className="form-check-label" htmlFor="activerMixeur">{enableMixeur ? "Retirer plaque mixeur" : "Activer plaque mixeur"}</label>
                 </div>
+
+                {enableMixeur ? (
+                    <input type="number" ref={temperatureVal} placeholder="Température" />
+                ) : ''}
 
                 <hr className="mt-3" />
 
